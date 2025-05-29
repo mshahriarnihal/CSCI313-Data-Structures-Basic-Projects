3891e74f4eed7488ee7b6878a19a03c8789b9032
Mubasshir Al Shahriar <shahriar.nihal@yahoo.com>
2025-05-28 22:25:03 -0400

    Update AVLTreeMapWithBalance.java

--- AVLTreeMapWithBalance.java
+++ AVLTreeMapWithBalance.java
@@ -1,11 +1,6 @@
 
-/* Student's Name: Mubasshir Al Shahriar, CUNYFirst ID: 24398818
-   Submitting To : Dr. Alla Rozovskaya
-   Course Name   : CSCI 313: Data Structures (Mon-Wednesday 3:10 p.m)
-
-   Assignment #3, Problem #2: In the AVL implementation in the chapter, each node stores the height of its subtree, which is an arbitrarily large integer. The space usage for an AVL tree can be reduced by instead storing the balance factor of a node, which is defined as the height of its left subtree minus the height of its right subtree. Thus, the balance factor of a node is always equal to 1, 0, or 1, except during an insertion or removal, when it may become temporarily equal to 2 or +2. Reimplement the AVLTreeMap class storing balance factors rather than subtree heights. */
-
-
+/*   Author's Name: Mubasshir Al Shahriar
+Relevant Course   : CSCI 313: Data Structures (Mon-Wednesday 3:10 p.m) */
 
 
 
